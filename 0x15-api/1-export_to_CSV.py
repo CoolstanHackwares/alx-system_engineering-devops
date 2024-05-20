702b7f3d2fbf8348a3d3841fe8e1f5141be237e6
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-05-20 15:26:59 -0700

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Fetches data from a REST API for an employee ID and exports it to CSV.
+Exports to-do list information for a given employee ID to CSV format.
 """
 
 import csv
@@ -11,40 +11,23 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: {} employee_id".format(sys.argv[0]))
         sys.exit(1)
-
+    
     employee_id = sys.argv[1]
-    base_url = 'https://jsonplaceholder.typicode.com/'
-    user_url = base_url + 'users/{}'.format(employee_id)
-    todos_url = base_url + 'todos?userId={}'.format(employee_id)
+    base_url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(
+        base_url + "users/{}".format(employee_id)
+    ).json()
+    username = user.get("username")
+    todos = requests.get(
+        base_url + "todos", params={"userId": employee_id}
+    ).json()
 
-    try:
-        user_data = requests.get(user_url).json()
-        todos_data = requests.get(todos_url).json()
-    except requests.exceptions.RequestException as e:
-        print("Error fetching data:", e)
-        sys.exit(1)
-
-    filename = "{}.csv".format(employee_id)
-    with open(filename, 'w', newline='') as csvfile:
-        fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS',
-                      'TASK_TITLE']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        writer.writeheader()
-        tasks_written = 0
-        for task in todos_data:
-            writer.writerow({
-                'USER_ID': employee_id,
-                'USERNAME': user_data['name'],
-                'TASK_COMPLETED_STATUS': str(task['completed']),
-                'TASK_TITLE': task['title']
-            })
-            tasks_written += 1
-
-    print("Data exported to", filename)
-
-    # Check if number of tasks written to CSV matches the total number of tasks
-    if tasks_written == len(todos_data):
-        print("Number of tasks in CSV: OK")
-    else:
-        print("Number of tasks in CSV: Incorrect")
+    with open("{}.csv".format(employee_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for todo in todos:
+            writer.writerow([
+                employee_id, 
+                username, 
+                todo.get("completed"), 
+                todo.get("title")
+            ])
